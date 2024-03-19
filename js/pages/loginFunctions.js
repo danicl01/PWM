cdd9ac7f75921d1c3dd0a07aa8cd8f6f8667bda5
JericoLuzardoMiranda <125827592+JericoLuzardoMiranda@users.noreply.github.com>
2024-03-19 21:48:44 +0000

    Fixed data validation in the login form

--- js/pages/loginFunctions.js
+++ js/pages/loginFunctions.js
@@ -1,48 +1,68 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("loginButton").addEventListener("click", function(event) {
-        event.preventDefault();
+    var email = document.getElementById("user-email");
+    var password = document.getElementById("user-password");
+    var loginForm = document.getElementById("loginForm");
 
-        var email = document.getElementById("user-email").value;
-        var password = document.getElementById("user-password").value;
+    function validateEmail() {
+        var emailValue = email.value.trim();
+        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-        clearErrors();
-
-        var isValid = validateLoginForm(email, password);
-        if (isValid) {
-            navigateToHomePage();
+        if (emailValue == "") {
+            showError("Por favor, rellena este campo", "emailError");
+            email.setCustomValidity("");
+            email.classList.add("input-error");
+        } else if (!emailRegex.test(emailValue)) {
+            showError("Por favor, introduzca un correo electrónico válido", "emailError");
+            email.setCustomValidity("Por favor, introduzca un correo electrónico válido");
+            email.classList.add("input-error");
+        } else {
+            showError("", "emailError");
+            email.setCustomValidity("");
+            email.classList.remove("input-error");
         }
-    });
+    }
+
+    function validatePassword() {
+        var passwordValue = password.value.trim();
+        var passwordRegex =  /^.{8,}$/;
+
+        if (passwordValue == "") {
+            showError("Por favor, rellena este campo", "passwordError");
+            password.setCustomValidity("");
+            password.classList.add("input-error");
+        } else if (!passwordRegex.test(passwordValue)) {
+            showError("Por favor, ingresa la contraseña válida", "passwordError");
+            password.setCustomValidity("Por favor, ingresa la contraseña válida");
+            password.classList.add("input-error");
+        } else {
+            showError("", "passwordError");
+            password.setCustomValidity("");
+            password.classList.remove("input-error");
+        }
+    }
+
+    function navigateToHomePage() {
+        var emailValue = email.value.trim();
+        var passwordValue = password.value.trim();
+
+        if (emailValue != "" && passwordValue != "") {
+            window.location.href = "homePage.html";
+        }
+    }
+
+    function showError(message, errorMessageId) {
+        var errorElement = document.getElementById(errorMessageId);
+        errorElement.innerText = message;
+    }
+
+    function validateForm(event) {
+        event.preventDefault();
+        validateEmail();
+        validatePassword();
+        navigateToHomePage();
+    }
+
+    email.addEventListener("blur", validateEmail);
+    password.addEventListener("blur", validatePassword);
+    loginForm.addEventListener("submit", validateForm);
 });
-
-function validateLoginForm(email, password) {
-    var isValid = true;
-
-    if (email == "") {
-        showError("Por favor, complete el campo de Email.", "emailError");
-        isValid = false;
-    }
-
-    if (password == "") {
-        showError("Por favor, complete el campo de Contraseña.", "passwordError");
-        isValid = false;
-    }
-
-    return isValid;
-}
-
-function showError(message, errorMessageId) {
-    var errorElement = document.getElementById(errorMessageId);
-    errorElement.innerText = message;
-}
-
-function clearErrors() {
-    var errorMessages = document.querySelectorAll(".error-message");
-    errorMessages.forEach(function(element) {
-        element.innerText = "";
-    });
-}
-
-function navigateToHomePage() {
-    var homePageUrl = "homePage.html";
-    window.location.href = homePageUrl;
-}
