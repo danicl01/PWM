87633da1d0c6a55996ade963e640a1b087644a54
JericoLuzardoMiranda <125827592+JericoLuzardoMiranda@users.noreply.github.com>
2024-03-28 16:18:37 +0000

    Implementation of JSON data creation function for registration page

--- js/pages/registerFunctions.js
+++ js/pages/registerFunctions.js
@@ -7,19 +7,19 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function validateInput(input, regex, errorMessageId, errorMessage) {
         const inputValue = input.value.trim();
-
+    
         if (inputValue == "") {
             showError("Por favor, rellena este campo", errorMessageId, input);
             input.classList.add("input-error");
         } else if (!regex.test(inputValue)) {
-            showError(errorMessage, errorMessageId);
+            showError(errorMessage, errorMessageId, input);
             input.classList.add("input-error");
         } else {
-            showError("", errorMessageId);
+            showError("", errorMessageId, input);
             input.classList.remove("input-error");
         }
     }
-
+    
     function validatePasswordMatch(password, confirmPassword, errorMessageId, errorMessage) {
         const passwordValue = password.value.trim();
         const confirmPasswordValue = confirmPassword.value.trim();
@@ -27,9 +27,11 @@ document.addEventListener("DOMContentLoaded", function() {
         if (passwordValue != confirmPasswordValue) {
             showError(errorMessage, errorMessageId);
             confirmPassword.classList.add("input-error");
+            password.classList.add("input-error");
         } else {
             showError("", errorMessageId);
             confirmPassword.classList.remove("input-error");
+            password.classList.remove("input-error");            
         }
     }
 
@@ -38,10 +40,36 @@ document.addEventListener("DOMContentLoaded", function() {
         errorElement.innerText = message;
     }
 
+    function createJSON() {
+        const usernameValue = document.getElementById("username").value;
+        const emailValue = document.getElementById("user-email").value;
+        const passwordValue = document.getElementById("user-password").value;
+        const confirmPasswordValue = document.getElementById("confirmPassword").value;
+
+        const formData = {
+            username: usernameValue,
+            email: emailValue,
+            password: passwordValue,
+            confirmPassword: confirmPasswordValue,
+        };
+
+        const jsonData = JSON.stringify(formData, null, 2);
+        const blob = new Blob([jsonData], { type: "application/json"});
+        const url = URL.createObjectURL(blob);
+
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = "userData.json";
+        document.body.appendChild(a);
+        a.click();
+        URL.revokeObjectURL(url);
+        a.remove(); 
+    }
+
     function validateForm(event) {
         event.preventDefault();
         validateInput(username, /^.{4,}$/, "usernameError", "El nombre de usuario es demasiado corto");
-        validateInput(email, /^[^\s@]+@[^\s@]+\.[^\s@]+$/, "emailError", "Por favor, introduzca un correo electrónico válido");
+        validateInput(email, /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/, "emailError", "Por favor, introduzca un correo electrónico válido");
         validateInput(password, /^.{8,}$/, "passwordError", "La contraseña debe tener al menos 8 caracteres");
         validateInput(confirmPassword, /^.{8,}$/, "confirmPasswordError", "La contraseña no cumple con los requisitos");
         validatePasswordMatch(password, confirmPassword, "confirmPasswordError", "Las contraseñas no coinciden");
@@ -52,16 +80,22 @@ document.addEventListener("DOMContentLoaded", function() {
         const confirmPasswordValue = confirmPassword.value.trim();
 
         if (usernameValue != "" && emailValue != "" && passwordValue != "" && confirmPasswordValue != "" && passwordValue === confirmPasswordValue) {
+            localStorage.setItem("loggedIn", "true");
             window.location.href = "confirmedRegisterPage.html";
         }
+
+        createJSON();
+
     }
 
     username.addEventListener("blur", () => validateInput(username, /^.{4,}$/, "usernameError", "El nombre de usuario es demasiado corto"));
-    email.addEventListener("blur", () => validateInput(email, /^[^\s@]+@[^\s@]+\.[^\s@]+$/, "emailError", "Por favor, introduzca un correo electrónico válido"));
+    email.addEventListener("blur", () => validateInput(email, /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/, "emailError", "Por favor, introduzca un correo electrónico válido"));
     password.addEventListener("blur", () => validateInput(password, /^.{8,}$/, "passwordError", "La contraseña debe tener al menos 8 caracteres"));
     confirmPassword.addEventListener("blur", () => {
         validateInput(confirmPassword, /^.{8,}$/, "confirmPasswordError", "La contraseña no cumple con los requisitos");
         validatePasswordMatch(password, confirmPassword, "confirmPasswordError", "Las contraseñas no coinciden");
     });
+
     registrationForm.addEventListener("submit", validateForm);
+
 });
